4108f93515e88c80b9ea0353fd6cd5045a5bcc30
Guy Mathieu <mathieu.guye@gmail.com>
2020-01-22 21:09:22 -0500

    Add project files.

---
+++ CustomSwitchDemo/CustomSwitchDemo/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CustomSwitchDemo.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomSwitchDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MySwitch_OnToggle(object sender, EventArgs e)
+        {
+            var control = (MySwitch)sender;
+
+            DisplayAlert("Alert", $"Switch is toggle: {control.IsSelected}", "OK");
+        }
+    }
+}
